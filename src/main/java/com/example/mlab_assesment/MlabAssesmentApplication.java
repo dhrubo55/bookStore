47e5f13d14937b2f35343b5e32b3c8a86e2d9e66
mohibul hassan <insular55@gmail.com>
2022-06-12 15:51:30 +0600

    adding spring boot project

---
+++ src/main/java/com/example/mlab_assesment/MlabAssesmentApplication.java
@@ -0,0 +1,15 @@
+package com.example.mlab_assesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlabAssesmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlabAssesmentApplication.class, args);
+
+    }
+
+
+}
---
+++ src/test/java/com/example/mlab_assesment/MlabAssesmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mlab_assesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlabAssesmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
